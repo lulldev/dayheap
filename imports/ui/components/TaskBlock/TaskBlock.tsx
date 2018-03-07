e91981d6f3ea272210999a536c7a411e700b2a7f
lulldev <lulldev@yandex.ru>
2018-03-07 12:21:31 +0300

    add titles for task block

--- imports/ui/components/TaskBlock/TaskBlock.tsx
+++ imports/ui/components/TaskBlock/TaskBlock.tsx
@@ -6,12 +6,15 @@ import {
   Col,
 } from 'react-bootstrap';
 
+import './TaskBlock.scss';
+
 import Task from '../Task';
 
 export default class TaskBlock extends React.Component<any, any> {
   public render() {
     return (
-      <Col lg={6}>
+      <Col lg={6} className="task-block">
+        <h2 className="title">{this.props.title}</h2>
         <Panel>
           <ListGroup>
             {this.props.tasks.map(task => (<Task task={task} key={task._id}/>))}
--- imports/ui/containers/TaskBoard/TaskBoard.tsx
+++ imports/ui/containers/TaskBoard/TaskBoard.tsx
@@ -15,18 +15,25 @@ import TaskBlock from '../../components/TaskBlock';
 
 export class TaskBoard extends React.Component<any, any> {
 
-  private tasks: any = [
-    { _id: 1, text: 'This is task 1', category: 'quot' },
-    { _id: 2, text: 'This is task 2', category: 'volgatech' },
-    { _id: 3, text: 'This is task 3', category: 'ood' },
-  ];
+  private tasks: any = {
+    heap: [
+      { _id: 1, category: 'quot', text: 'разобрать группу вопросов, посмотреть' +
+        ' конспекты, посмотреть вопросы, поставить цель на сдачу' },
+      { _id: 2, text: 'This is task 2', category: 'volgatech' },
+      { _id: 3, text: 'This is task 3', category: 'volgatech' },
+    ],
+    today: [
+      { _id: 1, text: 'This is task 1', category: 'quot' },
+      { _id: 2, text: 'This is task 2', category: 'volgatech' },
+    ],
+  };
   public render() {
     return (
       <div>
         <Grid>
           <Row className="show-grid">
-            <TaskBlock tasks={this.tasks}/>
-            <TaskBlock tasks={this.tasks}/>
+            <TaskBlock tasks={this.tasks.today} title="today"/>
+            <TaskBlock tasks={this.tasks.heap} title="heap"/>
           </Row>
         </Grid>
       </div>
